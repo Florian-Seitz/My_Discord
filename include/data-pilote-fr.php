07b0a9ac39601e0210751fb1f39abf954d3d5700
Florian Seitz <florian.seitz2@gmail.com>
2025-06-17 14:39:50 +0200

    Add files via upload

---
+++ include/data-pilote-fr.php
@@ -0,0 +1,499 @@
+<?php
+
+$pilote1 = [
+    'nom' => 'Lando Norris',
+    'image' => [
+        'images/pilotes/lando-norris.jpg',
+        'Photo de Lando Norris'
+    ],
+    'numero' => 4,
+    'equipe' => 'McLaren',
+    'pays' => 'Grande-Bretagne',
+    'podium' => 26,
+    'champion' => 0,
+    'date' => '1999-11-13',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Singapour', 'place' => '1er'],
+        ['id' => 2, 'annee' => 2024, 'lieu' => 'Allemagne', 'place' => '3e'],
+        ['id' => 3, 'annee' => 2024, 'lieu' => 'Brésil', 'place' => '2e'],
+        ['id' => 4, 'annee' => 2023, 'lieu' => 'Abu Dhabi', 'place' => '3e'],
+        ['id' => 5, 'annee' => 2023, 'lieu' => 'Italie', 'place' => '2e']
+    ],
+    'ids' => [
+        'norris',
+        'mclaren'
+    ]
+];
+
+$pilote2 = [
+    'nom' => 'Oscar Piastri',
+    'image' => [
+        'images/pilotes/oscar-piastri.jpg',
+        'Photo d\'Oscar Piastri'
+    ],
+    'numero' => 81,
+    'equipe' => 'McLaren',
+    'pays' => 'Australie',
+    'podium' => 10,
+    'champion' => 0,
+    'date' => '2001-05-06',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Monaco', 'place' => '1er'],
+        ['id' => 2, 'annee' => 2024, 'lieu' => 'Autriche', 'place' => '2e'],
+        ['id' => 3, 'annee' => 2024, 'lieu' => 'Italie', 'place' => '3e'],
+        ['id' => 4, 'annee' => 2023, 'lieu' => 'Abu Dhabi', 'place' => '2e'],
+        ['id' => 5, 'annee' => 2023, 'lieu' => 'Espagne', 'place' => '1er']
+    ],
+    'ids' => [
+        'piastri',
+        'mclaren'
+    ]
+];
+
+$pilote3 = [
+    'nom' => 'Charles Leclerc',
+    'image' => [
+        'images/pilotes/charles-leclerc.jpg',
+        'Photo de Charles Leclerc'
+    ],
+    'numero' => 16,
+    'equipe' => 'Ferrari',
+    'pays' => 'Monaco',
+    'podium' => 20,
+    'champion' => 0,
+    'date' => '1997-10-16',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Singapour', 'place' => '1er'],
+        ['id' => 2, 'annee' => 2024, 'lieu' => 'Canada', 'place' => '2e'],
+        ['id' => 3, 'annee' => 2024, 'lieu' => 'Autriche', 'place' => '3e'],
+        ['id' => 4, 'annee' => 2023, 'lieu' => 'Abu Dhabi', 'place' => '3e'],
+        ['id' => 5, 'annee' => 2023, 'lieu' => 'Italie', 'place' => '2e']
+    ],
+    'ids' => [
+        'leclerc',
+        'ferrari'
+    ]
+];
+
+$pilote4 = [
+    'nom' => 'Carlos Sainz',
+    'image' => [
+        'images/pilotes/carlos-sainz.jpg',
+        'Photo de Carlos Sainz'
+    ],
+    'numero' => 55,
+    'equipe' => 'Ferrari',
+    'pays' => 'Espagne',
+    'podium' => 14,
+    'champion' => 0,
+    'date' => '1994-09-01',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Brésil', 'place' => '1er'],
+        ['id' => 2, 'annee' => 2024, 'lieu' => 'Hongrie', 'place' => '3e'],
+        ['id' => 3, 'annee' => 2024, 'lieu' => 'France', 'place' => '2e'],
+        ['id' => 4, 'annee' => 2023, 'lieu' => 'Abu Dhabi', 'place' => '2e'],
+        ['id' => 5, 'annee' => 2023, 'lieu' => 'Monaco', 'place' => '1er']
+    ],
+    'ids' => [
+        'sainz',
+        'ferrari'
+    ]
+];
+
+$pilote5 = [
+    'nom' => 'Max Verstappen',
+    'image' => [
+        'images/pilotes/max-verstappen.jpg',
+        'Photo de Max Verstappen'
+    ],
+    'numero' => 1,
+    'equipe' => 'Red Bull Racing',
+    'pays' => 'Pays-Bas',
+    'podium' => 52,
+    'champion' => 3,
+    'date' => '1997-09-30',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Japon', 'place' => '1er'],
+        ['id' => 2, 'annee' => 2024, 'lieu' => 'Singapour', 'place' => '2e'],
+        ['id' => 3, 'annee' => 2024, 'lieu' => 'Canada', 'place' => '3e'],
+        ['id' => 4, 'annee' => 2023, 'lieu' => 'Mexique', 'place' => '1er'],
+        ['id' => 5, 'annee' => 2023, 'lieu' => 'Brésil', 'place' => '1er']
+    ],
+    'ids' => [
+        'verstappen',
+        'redbull'
+    ]
+];
+
+$pilote6 = [
+    'nom' => 'Sergio Perez',
+    'image' => [
+        'images/pilotes/sergio-perez.jpg',
+        'Photo de Sergio Perez'
+    ],
+    'numero' => 11,
+    'equipe' => 'Red Bull Racing',
+    'pays' => 'Mexique',
+    'podium' => 25,
+    'champion' => 0,
+    'date' => '1990-01-26',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Arabie Saoudite', 'place' => '1er'],
+        ['id' => 2, 'annee' => 2024, 'lieu' => 'Australie', 'place' => '2e'],
+        ['id' => 3, 'annee' => 2024, 'lieu' => 'Italie', 'place' => '3e'],
+        ['id' => 4, 'annee' => 2023, 'lieu' => 'Autriche', 'place' => '1er'],
+        ['id' => 5, 'annee' => 2023, 'lieu' => 'Espagne', 'place' => '2e']
+    ],
+    'ids' => [
+        'perez',
+        'redbull'
+    ]
+];
+
+$pilote7 = [
+    'nom' => 'George Russell',
+    'image' => [
+        'images/pilotes/george-russell.jpg',
+        'Photo de George Russell'
+    ],
+    'numero' => 63,
+    'equipe' => 'Mercedes',
+    'pays' => 'Grande-Bretagne',
+    'podium' => 10,
+    'champion' => 0,
+    'date' => '1998-02-15',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Australie', 'place' => '2e'],
+        ['id' => 2, 'annee' => 2024, 'lieu' => 'Canada', 'place' => '1er'],
+        ['id' => 3, 'annee' => 2023, 'lieu' => 'Brésil', 'place' => '3e'],
+        ['id' => 4, 'annee' => 2023, 'lieu' => 'Hongrie', 'place' => '2e'],
+        ['id' => 5, 'annee' => 2023, 'lieu' => 'Abu Dhabi', 'place' => '1er']
+    ],
+    'ids' => [
+        'russell',
+        'mercedes'
+    ]
+];
+
+$pilote8 = [
+    'nom' => 'Lewis Hamilton',
+    'image' => [
+        'images/pilotes/lewis-hamilton.jpg',
+        'Photo de Lewis Hamilton'
+    ],
+    'numero' => 44,
+    'equipe' => 'Mercedes',
+    'pays' => 'Grande-Bretagne',
+    'podium' => 42,
+    'champion' => 7,
+    'date' => '1985-01-07',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Singapour', 'place' => '2e'],
+        ['id' => 2, 'annee' => 2024, 'lieu' => 'Angleterre', 'place' => '1er'],
+        ['id' => 3, 'annee' => 2024, 'lieu' => 'Brésil', 'place' => '1er'],
+        ['id' => 4, 'annee' => 2023, 'lieu' => 'Abu Dhabi', 'place' => '2e'],
+        ['id' => 5, 'annee' => 2023, 'lieu' => 'Allemagne', 'place' => '3e']
+    ],
+    'ids' => [
+        'hamilton',
+        'mercedes'
+    ]
+];
+
+$pilote9 = [
+    'nom' => 'Fernando Alonso',
+    'image' => [
+        'images/pilotes/fernando-alonso.jpg',
+        'Photo de Fernando Alonso'
+    ],
+    'numero' => 14,
+    'equipe' => 'Aston Martin',
+    'pays' => 'Espagne',
+    'podium' => 103,
+    'champion' => 2,
+    'date' => '1981-07-29',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Monaco', 'place' => '2e'],
+        ['id' => 2, 'annee' => 2024, 'lieu' => 'Bahreïn', 'place' => '1er'],
+        ['id' => 3, 'annee' => 2024, 'lieu' => 'Émirats Arabes Unis', 'place' => '3e'],
+        ['id' => 4, 'annee' => 2023, 'lieu' => 'Canada', 'place' => '2e'],
+        ['id' => 5, 'annee' => 2023, 'lieu' => 'Bahreïn', 'place' => '1er']
+    ],
+    'ids' => [
+        'alonso',
+        'astonmartin'
+    ]
+];
+
+$pilote10 = [
+    'nom' => 'Lance Stroll',
+    'image' => [
+        'images/pilotes/lance-stroll.jpg',
+        'Photo de Lance Stroll'
+    ],
+    'numero' => 18,
+    'equipe' => 'Aston Martin',
+    'pays' => 'Canada',
+    'podium' => 3,
+    'champion' => 0,
+    'date' => '1998-10-29',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Sakhir ', 'place' => '3e'],
+        ['id' => 2, 'annee' => 2023, 'lieu' => 'Italie', 'place' => '3e'],
+        ['id' => 3, 'annee' => 2023, 'lieu' => 'Azerbaïdjan ', 'place' => '3e'],
+        ['id' => 4, 'annee' => 2022, 'lieu' => 'Brésil', 'place' => '2e'],
+        ['id' => 5, 'annee' => 2021, 'lieu' => 'France', 'place' => '1er']
+    ],
+    'ids' => [
+        'stroll',
+        'astonmartin'
+    ]
+];
+
+$pilote11 = [
+    'nom' => 'Pierre Gasly',
+    'image' => [
+        'images/pilotes/pierre-gasly.jpg',
+        'Photo de Pierre Gasly'
+    ],
+    'numero' => 10,
+    'equipe' => 'Alpine',
+    'pays' => 'France',
+    'podium' => 3,
+    'champion' => 0,
+    'date' => '1996-02-07',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'São Paulo', 'place' => '3e'],
+        ['id' => 2, 'annee' => 2023, 'lieu' => 'Pays-Bas', 'place' => '3e'],
+        ['id' => 3, 'annee' => 2021, 'lieu' => 'Azerbaïdjan', 'place' => '3e'],
+        ['id' => 4, 'annee' => 2020, 'lieu' => 'Italie', 'place' => '1er'],
+        ['id' => 5, 'annee' => 2019, 'lieu' => 'Brésil', 'place' => '2e']
+    ],
+    'ids' => [
+        'gasly',
+        'alpine'
+    ]
+];
+
+$pilote12 = [
+    'nom' => 'Esteban Ocon',
+    'image' => [
+        'images/pilotes/esteban-ocon.jpg',
+        'Photo de Esteban Ocon'
+    ],
+    'numero' => 31,
+    'equipe' => 'Alpine',
+    'pays' => 'France',
+    'podium' => 2,
+    'champion' => 0,
+    'date' => '1996-09-17',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'São Paulo', 'place' => '2e'],
+        ['id' => 2, 'annee' => 2023, 'lieu' => 'Monaco', 'place' => '3e'],
+        ['id' => 3, 'annee' => 2021, 'lieu' => 'Hongrie', 'place' => '1er'],
+        ['id' => 4, 'annee' => 2020, 'lieu' => 'Sakhir', 'place' => '2e'],
+        ['id' => 5, 'annee' => 2019, 'lieu' => 'Brésil', 'place' => '2e']
+    ],
+    'ids' => [
+        'ocon',
+        'alpine'
+    ]
+];
+
+$pilote13 = [
+    'nom' => 'Nico Hülkenberg',
+    'image' => [
+        'images/pilotes/nico-hulkenberg.jpg',
+        'Photo de Nico Hülkenberg'
+    ],
+    'numero' => 27,
+    'equipe' => 'Haas',
+    'pays' => 'Allemagne',
+    'podium' => 0,
+    'champion' => 0,
+    'date' => '1987-08-19',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2023, 'lieu' => 'Japon', 'place' => '2e'],
+        ['id' => 2, 'annee' => 2022, 'lieu' => 'France', 'place' => '1er'],
+        ['id' => 3, 'annee' => 2022, 'lieu' => 'Brésil', 'place' => '2e'],
+        ['id' => 4, 'annee' => 2020, 'lieu' => 'Canada', 'place' => '1e']
+    ],
+    'ids' => [
+        'hulkenberg',
+        'haas'
+    ]
+];
+
+$pilote14 = [
+    'nom' => 'Kevin Magnussen',
+    'image' => [
+        'images/pilotes/kevin-magnussen.jpg',
+        'Photo de Kevin Magnussen'
+    ],
+    'numero' => 20,
+    'equipe' => 'Haas',
+    'pays' => 'Danemark',
+    'podium' => 1,
+    'champion' => 0,
+    'date' => '1992-10-05',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2023, 'lieu' => 'Australie', 'place' => '2e'],
+        ['id' => 2, 'annee' => 2022, 'lieu' => 'Brésil', 'place' => '2e'],
+        ['id' => 3, 'annee' => 2020, 'lieu' => 'France', 'place' => '1er'],
+        ['id' => 4, 'annee' => 2014, 'lieu' => 'Japon', 'place' => '2e']
+    ],
+    'ids' => [
+        'magnussen',
+        'haas'
+    ]
+];
+
+$pilote15 = [
+    'nom' => 'Yuki Tsunoda',
+    'image' => [
+        'images/pilotes/yuki-tsunoda.jpg',
+        'Photo de Yuki Tsunoda'
+    ],
+    'numero' => 22,
+    'equipe' => 'Racing Bull',
+    'pays' => 'Japon',
+    'podium' => 0,
+    'champion' => 0,
+    'date' => '2000-05-11',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2023, 'lieu' => 'Canada', 'place' => '1e'],
+        ['id' => 2, 'annee' => 2022, 'lieu' => 'France', 'place' => '1er'],
+        ['id' => 3, 'annee' => 2022, 'lieu' => 'Brésil', 'place' => '2e'],
+        ['id' => 4, 'annee' => 2021, 'lieu' => 'Japon', 'place' => '2e']
+    ],
+    'ids' => [
+        'tsunoda',
+        'racingbull'
+    ]
+];
+
+$pilote16 = [
+    'nom' => 'Daniel Ricciardo',
+    'image' => [
+        'images/pilotes/daniel-ricciardo.jpg',
+        'Photo de Daniel Ricciardo'
+    ],
+    'numero' => 3,
+    'equipe' => 'Racing Bull',
+    'pays' => 'Australie',
+    'podium' => 32,
+    'champion' => 0,
+    'date' => '1989-07-01',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2021, 'lieu' => 'Italie', 'place' => '1er'],
+        ['id' => 2, 'annee' => 2020, 'lieu' => 'Émilie-Romagne', 'place' => '3e'],
+        ['id' => 3, 'annee' => 2020, 'lieu' => 'Eifel', 'place' => '3e'],
+        ['id' => 4, 'annee' => 2018, 'lieu' => 'Monaco', 'place' => '1er'],
+        ['id' => 5, 'annee' => 2018, 'lieu' => 'Chine', 'place' => '1er']
+    ],
+    'ids' => [
+        'ricciardo',
+        'racingbull'
+    ]
+];
+
+$pilote17 = [
+    'nom' => 'Alexander Albon',
+    'image' => [
+        'images/pilotes/alex-albon.jpg',
+        'Photo de Alexander Albon'
+    ],
+    'numero' => 23,
+    'equipe' => 'Williams',
+    'pays' => 'Thaïlande',
+    'podium' => 2,
+    'champion' => 0,
+    'date' => '1996-03-23',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2023, 'lieu' => 'Toscane ', 'place' => '3e'],
+        ['id' => 2, 'annee' => 2023, 'lieu' => 'Bahreïn', 'place' => '3e'],
+        ['id' => 3, 'annee' => 2022, 'lieu' => 'Brésil', 'place' => '2e'],
+        ['id' => 4, 'annee' => 2020, 'lieu' => 'France', 'place' => '1er'],
+        ['id' => 5, 'annee' => 2020, 'lieu' => 'Japon', 'place' => '2e']
+    ],
+    'ids' => [
+        'albon',
+        'williams'
+    ]
+];
+
+$pilote18 = [
+    'nom' => 'Franco Colapinto',
+    'image' => [
+        'images/pilotes/franco-colapinto.jpg',
+        'Photo de Franco Colapinto'
+    ],
+    'numero' => 2,
+    'equipe' => 'Williams',
+    'pays' => 'Argentine',
+    'podium' => 0,
+    'champion' => 0,
+    'date' => '2003-05-27',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2022, 'lieu' => 'Brésil', 'place' => '3e'],
+        ['id' => 2, 'annee' => 2021, 'lieu' => 'France', 'place' => '3e']
+    ],
+    'ids' => [
+        'colapinto',
+        'williams'
+    ]
+];
+
+$pilote19 = [
+    'nom' => 'Guanyu Zhou',
+    'image' => [
+        'images/pilotes/guanyu-zhou.jpg',
+        'Photo de Guanyu Zhou'
+    ],
+    'numero' => 24,
+    'equipe' => 'Stake Kick Sauber',
+    'pays' => 'Chine',
+    'podium' => 0,
+    'champion' => 0,
+    'date' => '1999-05-30',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2024, 'lieu' => 'Canada', 'place' => '1e'],
+        ['id' => 2, 'annee' => 2022, 'lieu' => 'Brésil', 'place' => '2e'],
+        ['id' => 3, 'annee' => 2021, 'lieu' => 'Japon', 'place' => '2e'],
+        ['id' => 4, 'annee' => 2020, 'lieu' => 'France', 'place' => '1er']
+    ],
+    'ids' => [
+        'zhou',
+        'stake'
+    ]
+];
+
+$pilote20 = [
+    'nom' => 'Valtteri Bottas',
+    'image' => [
+        'images/pilotes/valtteri-bottas.jpg',
+        'Photo de Valtteri Bottas'
+    ],
+    'numero' => 77,
+    'equipe' => 'Stake Kick Sauber',
+    'pays' => 'Finlande',
+    'podium' => 67,
+    'champion' => 0,
+    'date' => '1989-08-28',
+    'dernierpodium' => [
+        ['id' => 1, 'annee' => 2021, 'lieu' => 'Arabie Saoudite', 'place' => '3e'],
+        ['id' => 2, 'annee' => 2021, 'lieu' => 'Mexique', 'place' => '3e'],
+        ['id' => 3, 'annee' => 2021, 'lieu' => 'Turquie', 'place' => '1er'],
+        ['id' => 4, 'annee' => 2021, 'lieu' => 'Russie', 'place' => '3e'],
+        ['id' => 5, 'annee' => 2021, 'lieu' => 'Italie', 'place' => '3e']
+    ],
+    'ids' => [
+        'bottas',
+        'stake'
+    ]
+];
+
+$categoriePilote = [
+    $pilote1,$pilote2, $pilote3, $pilote4, $pilote5, $pilote6, $pilote7, $pilote8, $pilote9, $pilote10,
+    $pilote11, $pilote12, $pilote13, $pilote14, $pilote15, $pilote16, $pilote17, $pilote18, $pilote19, $pilote20
+];
---
+++ include/data-serveur.php
@@ -0,0 +1,29 @@
+<?php
+
+include('include/data-compte.php');
+
+$serv1 = [
+    'nom' => 'Eternity Feniks',
+    'imgprofil' => 'phoenix-logo.png',
+    'message' => [
+        'msg1' => [
+            'compte' => $compte,
+            'date' => '12/02/2025 - 08:25',
+            'message' => 'salut ça va ?'
+        ]
+    ]
+];
+
+$serv2 = [
+    'nom' => 'Eternity Feniks',
+    'imgprofil' => 'phoenix-logo.png',
+    'message' => [
+        'msg1' => [
+            'compte' => $compte,
+            'date' => '12/02/2025 - 08:25',
+            'message' => 'salut ça va ?'
+        ]
+    ]
+];
+
+$serveurs = [$serv1, $serv2];
